0150e9ed0e70354e385ad6b30ecd70b5bd8d6c66
Mark Kerzner <markkerzner@gmail.com>
2011-11-23 16:38:34 -0600

    clean scripts

--- run_in_hadoop.sh
+++ run_in_hadoop.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+hadoop fs -rmr /hadoopilluminated/test-data/ch01
+hadoop fs -mkdir /hadoopilluminated/test-data/ch01
+hadoop fs -copyFromLocal  test-data/ch01/moby-dick.txt /hadoopilluminated/test-data/ch01
+hadoop fs -rmr /hadoopilluminated/test-output/ch01
 hadoop jar \
-target/HadoopInPracticeCode-1.0-SNAPSHOT-jar-with-dependencies.jar \
-/test-data/ch1/moby-dick.txt \
-/test-output/ch1 
+target/HadoopIlluminated-1.0-SNAPSHOT-jar-with-dependencies.jar \
+/hadoopilluminated/test-data/ch01 \
+/hadoopilluminated/test-output/ch01
